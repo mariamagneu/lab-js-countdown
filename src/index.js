dda62e7ba937bdcfacf322cb5e897b7debd8b783
maria magdalena <neumariamagdalena@gmail.com>
2024-06-13 16:03:00 +0200

    all but bonus

--- src/index.js
+++ src/index.js
@@ -1,38 +1,46 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
-
-
+const timeLeft = document.querySelector("#time");
+const toast = document.querySelector(".toast");
+let show = document.querySelector(".show");
 
 // ITERATION 1: Add event listener to the start button
-
 // Your code goes here ...
-
-
-
+const startBtn = document.querySelector("#start-btn");
+startBtn.addEventListener("click", startCountdown);
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
 
-
   // Your code goes here ...
+  const intervalID = setInterval(() => {
+    if (remainingTime > 0) {
+      console.log("THIS IS THE COUNTDOWN HAPPENING");
+      remainingTime--;
+      timeLeft.innerText = remainingTime;
+      startBtn.disabled = true;
+    } else if (remainingTime === 0) {
+      showToast();
+      clearInterval(intervalID);
+    }
+  }, 1000);
 }
 
-
-
-
 // ITERATION 3: Show Toast
+
 function showToast(message) {
   console.log("showToast called!");
 
+  toast.classList.toggle("show");
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000);
+
   // Your code goes here ...
-
-
-
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
 }
